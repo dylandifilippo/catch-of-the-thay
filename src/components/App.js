93d1517bc7ac2aa7bc0cb1127a1ad5002833e7b5
Dylan Di Filppo <dylan.di.filippo@gmail.com>
2020-06-03 11:06:22 +0200

    Finished the localStorage order State

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,11 @@ class App extends Component {
 
   componentDidMount() {
     const { params } = this.props.match;
+    // first reinstate our localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: 'fishes',
@@ -24,7 +29,10 @@ class App extends Component {
   }
 
   componentDidUpdate() {
-    console.log(this.state.order);
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    );
   }
 
   componentWillUnmount() {
--- src/components/Order.js
+++ src/components/Order.js
@@ -7,7 +7,9 @@ class Order extends Component {
   renderOrder = key => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
-    const isAvailable = fish.status === 'available';
+    const isAvailable = fish && fish.status === 'available';
+    // Make sure the fish is loaded before we continue
+    if (!fish) return null;
     if (!isAvailable) {
       return <li>Sorry {fish ? fish.name : 'fish'} is no longer available </li>;
     }
